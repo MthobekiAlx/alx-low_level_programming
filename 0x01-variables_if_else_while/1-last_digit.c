0f3257bc55bc2d52e41d65b3ede4326200742fc7
Mthobezi <mthobekisikhosang@gmail.com>
2023-02-17 20:35:44 +0200

    two try

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,14 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
-/**
- *main - print lowercase a-z
- *Return: Always 0 (Success)
- */
-
+/* betty style doc for function main goes there */
 int main(void)
 {
-	char alpha = 'a';
+	int n;
 
-	while (alpha <= 'z')
-	{
-		putchar(alpha);
-		alpha++;
-	}
-	putchar('\n');
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	return (0);
 }
