ee901ec9f975c4872dca1c8f626f83a45cb99f89
Mthobezi <mthobekisikhosang@gmail.com>
2023-03-08 10:31:53 +0200

    hope its right

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+0-puts_recursion.c [unix] (01:59 01/01/1970)                           0,0-1 All
+"0-puts_recursion.c" [New]
+
