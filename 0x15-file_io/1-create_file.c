f8e0d8c9db0bb4f8f2d62208fd33328cf82cdb8d
Mthobezi <mthobekisikhosang@gmail.com>
2023-05-08 23:42:57 +0200

    file_io file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * create_file -creates an array of chars, and initializes
+ *
+ * @text_content: is a NULL terminated string to write to the file
+ * @filename: is the name of the file to create
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
 
-int create_file(const char *filename, char *text_content) {
-    int fd, bytes_written;
-    mode_t mode = S_IRUSR | S_IWUSR; // rw-------
+	if (filename == NULL)
+		return (-1);
 
-    if (filename == NULL) {
-        return -1;
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-    if (fd == -1) {
-        return -1;
-    }
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-    if (text_content != NULL) {
-        bytes_written = write(fd, text_content, strlen(text_content));
-        if (bytes_written == -1) {
-            close(fd);
-            return -1;
-        }
-    }
+	if (o == -1 || w == -1)
+		return (-1);
 
-    close(fd);
-    return 1;
+	close(o);
+
+	return (1);
 }
-
