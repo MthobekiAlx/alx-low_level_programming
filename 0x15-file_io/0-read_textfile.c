c65ca5f133ce6cf72f15dda7926ea933d40f0db9
Mthobezi <mthobekisikhosang@gmail.com>
2023-05-08 23:49:10 +0200

    file_io file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,30 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * read_textfile - read a certain size and prints to std output
+ * @filename: file to read from
+ * @letters: size to read
+ * Return: actual size read and printed
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        ssize_t o, r, w;
-        char *buffer;
+	int fd; /* file descriptor */
+	ssize_t n_read, n_wrote;
+	char *buffer;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        buffer = malloc(sizeof(char) * letters);
-        if (buffer == NULL)
-                return (0);
+	/* open */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-        o = open(filename, O_RDONLY);
-        r = read(o, buffer, letters);
-        w = write(STDOUT_FILENO, buffer, r);
+	/* malloc buffer */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-        if (o == -1 || r == -1 || w == -1 || w != r)
-        {
-                free(buffer);
-                return (0);
-        }
+	/* read */
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-        free(buffer);
-        close(o);
+	/* write */
+	n_wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (n_wrote == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-        return (w);
+	close(fd);
+	return (n_read);
+
 }
