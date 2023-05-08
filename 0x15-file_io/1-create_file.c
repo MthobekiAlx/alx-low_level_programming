a98581179f106a1d45361ed62c81930811a3915d
Mthobezi <mthobekisikhosang@gmail.com>
2023-05-08 23:39:07 +0200

    file_io file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,25 +1,33 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-int create_file(const char *filename, char *text_content)
-{
-	int o, w, len = 0;
+int create_file(const char *filename, char *text_content) {
+    int fd, bytes_written;
+    mode_t mode = S_IRUSR | S_IWUSR; // rw-------
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL) {
+        return -1;
+    }
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+    if (fd == -1) {
+        return -1;
+    }
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+    if (text_content != NULL) {
+        bytes_written = write(fd, text_content, strlen(text_content));
+        if (bytes_written == -1) {
+            close(fd);
+            return -1;
+        }
+    }
 
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
-	return (1);
+    close(fd);
+    return 1;
 }
+
