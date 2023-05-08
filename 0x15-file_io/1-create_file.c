be8342de861d626c129bc1597d9675c7e74788a8
Mthobezi <mthobekisikhosang@gmail.com>
2023-05-08 23:40:26 +0200

    file_io file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 int create_file(const char *filename, char *text_content) {
     int fd, bytes_written;
