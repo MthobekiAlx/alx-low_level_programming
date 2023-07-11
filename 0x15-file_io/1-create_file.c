27484426ac64f2b56b178cfb76c7813acfda5a5a
mthobezi <mthobekisikhosanag@gmail.com>
2023-07-11 07:55:03 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,55 @@
 #include "main.h"
+
 /**
- * create_file -creates an array of chars, and initializes
- *
- * @text_content: is a NULL terminated string to write to the file
- * @filename: is the name of the file to create
- *
- * Return: 1 on success, -1 on failure
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
+ * create_file - creates file with permissions rw------- and writes content in
+ * if file already exists, don't change permissions and just truncate it
+ * @filename: name to give to new file
+ * @text_content: writes this content into file
+ * Return: 1 on success, -1 on error
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd;
+	int n_wrote;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
+	/* create with permissions if file doesn't exist, else truncate */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	/* if nothing to write, just return newly created file */
+	if (!text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		close(fd);
+		return (1);
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
 		return (-1);
+	}
 
-	close(o);
-
+	close(fd);
 	return (1);
 }
